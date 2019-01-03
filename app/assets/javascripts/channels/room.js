90a3545484482e4356c4918471f9d2768ca4c98f
yano <biblioteca.financial.nine@gmail.com>
2019-01-03 17:25:02 +0900

    Youtubeまで完成

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -8,9 +8,11 @@ App.room = App.cable.subscriptions.create("RoomChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received: function(data) {
+  received: function(message) {
     console.log('received!')
-    alert(data)
+    const messages = document.getElementById('messages')
+    messages.innerHTML += message
+
     // Called when there's incoming data on the websocket for this channel
   },
 
@@ -21,5 +23,11 @@ App.room = App.cable.subscriptions.create("RoomChannel", {
 
 // 
 document.addEventListener('DOMContentLoaded', function(){
-  input = document.getElementById('chat-input')
+  const input = document.getElementById('chat-input')
+  const button = document.getElementById('button')
+  button.addEventListener('click', function(){
+    const content = input.value
+    App.room.speak(content)
+    input.value = ''
+  })
 })
